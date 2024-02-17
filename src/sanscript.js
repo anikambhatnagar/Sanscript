3c9723fdeb2a56f0fc76e78bcd6c7a8a6a3fa92e
gauravbasu03 <112521320+gauravbasu03@users.noreply.github.com>
2024-02-16 22:43:23 -0800

    added main

--- src/sanscript.js
+++ src/sanscript.js
@@ -1,1 +1,15 @@
-console.log("Shuru Kare!");
+import * as fs from "node:fs/promises"
+import process from "node:process"
+import parse from "./parser.js"
+
+if (process.argv.length !== 3) {
+  console.log('Must have exactly one argument: the filename of the program to compile.')
+} else {
+  try {
+    const buffer = await fs.readFile(process.argv[2])
+    parse(buffer.toString())
+    console.log("Syntax ok")
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`)
+  }
+}
