29eadb52846b8f00e15b1b460f6e82184a031a39
gauravbasu03 <gauravbasu03@gmail.com>
2024-02-19 14:29:51 -0800

    fix

--- src/sanscript.js
+++ src/sanscript.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import parse from "./parser.js"
 try {
     parse("likhana(1)")
@@ -12,20 +11,3 @@ catch (e) {
 
 
 
-=======
-import * as fs from "node:fs/promises"
-import process from "node:process"
-import parse from "./parser.js"
-
-if (process.argv.length !== 3) {
-  console.log('Must have exactly one argument: the filename of the program to compile.')
-} else {
-  try {
-    const buffer = await fs.readFile(process.argv[2])
-    parse(buffer.toString())
-    console.log("Syntax ok")
-  } catch (e) {
-    console.error(`\u001b[31m${e}\u001b[39m`)
-  }
-}
->>>>>>> 3c9723fdeb2a56f0fc76e78bcd6c7a8a6a3fa92e
