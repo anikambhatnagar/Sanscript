a968f644791b697a7ea8fa92bda9ee09eca6d922
gauravbasu03 <gauravbasu03@gmail.com>
2024-02-19 14:25:56 -0800

    added js

--- src/sanscript.js
+++ src/sanscript.js
@@ -1,1 +1,13 @@
-console.log("Shuru Kare!");
+import parse from "./parser.js"
+try {
+    parse("likhana(1)")
+    console.log("Syntax is ok")
+
+}
+catch (e) {
+    console.log("\x1b[31m%s\x1b[0m" , e.message);
+    process.exit(1)
+}
+
+
+
