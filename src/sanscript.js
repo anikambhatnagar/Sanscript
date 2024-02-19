157a915f548bd6d408dc3cad43d38374ac5c832c
gauravbasu03 <gauravbasu03@gmail.com>
2024-02-19 14:32:30 -0800

    from slack

--- src/sanscript.js
+++ src/sanscript.js
@@ -1,12 +1,17 @@
+import * as fs from "node:fs/promises"
+import process from "node:process"
 import parse from "./parser.js"
-try {
-    parse("likhana(1)")
-    console.log("Syntax is ok")
 
-}
-catch (e) {
-    console.log("\x1b[31m%s\x1b[0m" , e.message);
-    process.exit(1)
+if (process.argv.length !== 3) {
+  console.log('Must have exactly one argument: the filename of the program to compile.')
+} else {
+  try {
+    const buffer = await fs.readFile(process.argv[2])
+    parse(buffer.toString())
+    console.log("Syntax ok")
+  } catch (e) {
+    console.error(`\u001b[31m${e}\u001b[39m`)
+  }
 }
 
 
