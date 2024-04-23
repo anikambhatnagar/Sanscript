388a06fab66fadda8360bdd71fe3b6a4f4bac4ac
Anika Bhatnagar <90007703+anikambhatnagar@users.noreply.github.com>
2024-04-22 18:43:22 -0700

    updates test for 181 and 184 error - return true and false

--- test/analyzer.test.js
+++ test/analyzer.test.js
@@ -1,3 +1,39 @@
+/*import assert from "node:assert/strict"
+import parse from "../src/parser.js"
+import analyze from "../src/analyzer.js"
+
+// Programs that are semantically correct
+const semanticChecks = [
+  ["hello world", `likhana("नमस्ते दुनिया");`],
+  ["variable declarations", "sankhya var1 = 15;"],
+  ["while statement", "jabtaki (5) { likhana 2; }"],
+  [
+    "while statement with variable",
+    "sankhya x = 15; jabtaki (x < 25) { likhana x + 2; x = x + 1; }",
+  ],
+  ["function declaration", "prakriya doubleIt(x) { laana x * 2; }"],
+  ["long return", "prakriya f() { laana saty; }"]
+]
+
+const semanticErrors = [
+  ["undeclared id", "likhana(x);", /Identifier x not declared/],
+  ["return type mismatch", "prakriya f() { laana asaty; }"]
+]
+
+describe("The analyzer", () => {
+  for (const [scenario, source] of semanticChecks) {
+    it(`recognizes ${scenario}`, () => {
+      assert.ok(analyze(parse(source)))
+    })
+  }
+  for (const [scenario, source, errorMessagePattern] of semanticErrors) {
+    it(`throws on ${scenario}`, () => {
+      assert.throws(() => analyze(parse(source)), errorMessagePattern)
+    })
+  }
+})
+*/
+
 import assert from "node:assert/strict"
 import parse from "../src/parser.js"
 import analyze from "../src/analyzer.js"
@@ -12,10 +48,12 @@ const semanticChecks = [
     "sankhya x = 15; jabtaki (x < 25) { likhana x + 2; x = x + 1; }",
   ],
   ["function declaration", "prakriya doubleIt(x) { laana x * 2; }"],
+  ["long return", "prakriya f() { laana saty; }"]
 ]
 
 const semanticErrors = [
   ["undeclared id", "likhana(x);", /Identifier x not declared/],
+  ["return type mismatch", "prakriya f() { laana asaty; }"]
 ]
 
 describe("The analyzer", () => {
